99bdd1575d3b3d15b53eaa281d22c721fdf1f762
Lapidebr <lucasdeusex@gmail.com>
2020-06-02 10:19:26 -0300

    Update currency.py
    Melhoria de código

--- currency.py
+++ currency.py
@@ -2,15 +2,12 @@
 import json, requests
 
 # bloco para realizar o GET online
-
-#req = requests.get("https://economia.awesomeapi.com.br/json/all")
-#resposta = json.loads(req.text)
-
+req = requests.get("https://economia.awesomeapi.com.br/json/all")
+resposta = json.loads(req.text)
 # Fim bloco para realizar o GET online
 
-resposta = json.loads(open('currency.json').read())
+#resposta = json.loads(open('currency.json').read()) # para teste local com leitura no arquivo currency.json ao usar comente o bloco "GET online"
+
 colecttime = (resposta['USD']['create_date'])
-
 realtime = (colecttime[8:10]+"/"+colecttime[5:7]+"/"+colecttime[0:4]+"-"+colecttime[11:19]) # Este campo formata da no padrao brasileiro formato original:2018-09-03 10:40:09
-
 print ('Dólar R${}\nEuro R${}\nLibra R${}\nBitcoin R${}\nGenerate by economia.awesomeapi.com.br at {}(USD)'.format(str(resposta['USD']['high']),str(resposta['EUR']['high']),str(resposta['GBP']['high']),str(resposta['BTC']['high']),realtime))
